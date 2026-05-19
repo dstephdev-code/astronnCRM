c0fb8452a97a9e0da1f6640de36153b680e572e4
Denis <d.steph.dev@gmail.com>
2026-05-19 21:37:54 +0400

    Add swagger and deleted unneccesery start template file

--- astronnCRM.Api/Program.cs
+++ astronnCRM.Api/Program.cs
@@ -7,24 +7,24 @@ namespace astronnCRM.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo { Title = "astronnCRM.API", Version = "v1" });
+            });
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "astronnCRM API v1"); });
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllerRoute(name:"areas", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
