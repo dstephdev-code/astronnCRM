528707c2ac3bb9b239cdc1bbb5f4ee5bf6c43f2d
Denis <d.steph.dev@gmail.com>
2026-05-20 19:48:48 +0400

    Simple registration provided

--- astronnCRM.Api/Program.cs
+++ astronnCRM.Api/Program.cs
@@ -1,6 +1,8 @@
 using astronnCRM.DataAccess;
+using astronnCRM.Model.IdentityModels;
 using astronnCRM.Service;
 using astronnCRM.Service.IService;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
@@ -21,6 +23,10 @@ namespace astronnCRM.Api
                 options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo { Title = "astronnCRM.API", Version = "v1" });
             });
             builder.Services.AddOpenApi();
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDBContext>()
+                .AddDefaultTokenProviders();
 
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
--- astronnCRM.Service/AuthenticationService.cs
+++ astronnCRM.Service/AuthenticationService.cs
@@ -20,6 +20,7 @@ namespace astronnCRM.Service
             var user = new ApplicationUser
             {
                 Email = model.Email,
+                UserName = model.Email,
 
                 FirstName = model.FirstName,
                 LastName = model.LastName,
@@ -28,7 +29,7 @@ namespace astronnCRM.Service
                 DateOfRegistration = DateTime.UtcNow
             };
 
-            var result = await userManager.CreateAsync(user);
+            var result = await userManager.CreateAsync(user, model.Password);
             return result.Succeeded ? true : throw new Exception("Unable to create user; Errors: " + result.Errors);
         }
 
