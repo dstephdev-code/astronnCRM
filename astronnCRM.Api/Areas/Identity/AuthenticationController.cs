307a76fdac0c5c2f729812f2e886a35af1d1c426
Denis <d.steph.dev@gmail.com>
2026-05-20 18:57:58 +0400

    Auth controller methods params updated

--- astronnCRM.Api/Areas/Identity/AuthenticationController.cs
+++ astronnCRM.Api/Areas/Identity/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using astronnCRM.Model.InputModels;
+using astronnCRM.Service.IService;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 
 namespace astronnCRM.Api.Areas.Identity
@@ -7,60 +9,53 @@ namespace astronnCRM.Api.Areas.Identity
     [DisplayName("Authentication controller")]
     [Route("api/[area]/[controller]")]
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(IAuthenticationService authenticationService) : ControllerBase
     {
-        private readonly IAuthenticationService _authentificationService;
-
-        public AuthenticationController(IAuthenticationService authenticationService)
-        {
-            _authentificationService = authenticationService;
-        }
-
         [HttpPost("login")]
         [DisplayName("Login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        public async Task<IActionResult> Login([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.LoginAsync(request);
+            var response = await authenticationService.LoginAsync(model);
             return Ok(response);
         }
 
         [HttpPost("register")]
         [DisplayName("Register")]
-        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.RegisterAsync(request);
+            var response = await authenticationService.RegisterAsync(model);
             return Ok(response);
         }
 
         [HttpPost("forgot-password")]
         [DisplayName("Forgot password")]
-        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request)
+        public async Task<IActionResult> ForgotPassword([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.ForgotPasswordAsync(request);
+            var response = await authenticationService.ForgotPasswordAsync(model);
             return Ok(response);
         }
 
         [HttpPost("reset-password")]
         [DisplayName("Reset password")]
-        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request)
+        public async Task<IActionResult> ResetPassword([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.ResetPasswordAsync(request);
+            var response = await authenticationService.ResetPasswordAsync(model);
             return Ok(response);
         }
 
         [HttpPost("change-password")]
         [DisplayName("Change password")]
-        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request)
+        public async Task<IActionResult> ChangePassword([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.ChangePasswordAsync(request);
+            var response = await authenticationService.ChangePasswordAsync(model);
             return Ok(response);
         }
 
         [HttpPost("refresh-token")]
         [DisplayName("Refresh token")]
-        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequest request)
+        public async Task<IActionResult> RefreshToken([FromBody] ApplicationUserInputModel model)
         {
-            var response = await _authentificationService.RefreshTokenAsync(request);
+            var response = await authenticationService.RefreshTokenAsync(model);
             return Ok(response);
         }
     }
