078b39f8e817332d47ca6fded51f957d71bf4849
Denis <d.steph.dev@gmail.com>
2026-05-20 18:50:53 +0400

    Register user method added

--- astronnCRM.Service/AuthenticationService.cs
+++ astronnCRM.Service/AuthenticationService.cs
@@ -1,40 +1,53 @@
-﻿using astronnCRM.Model.InputModels;
+﻿using astronnCRM.Model.IdentityModels;
+using astronnCRM.Model.InputModels;
 using astronnCRM.Service.IService;
+using Microsoft.AspNetCore.Identity;
 
 namespace astronnCRM.Service
 {
-    public class AuthenticationService : IAuthenticationService
+    public class AuthenticationService(UserManager<ApplicationUser> userManager) : IAuthenticationService
     {
-        public AuthenticationService()
-        {
-        }
-
-        public Task<bool> ChangePasswordAsync(ApplicationUserInputModel model)
+        public async Task<bool> LoginAsync(ApplicationUserInputModel model)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> ForgotPasswordAsync(ApplicationUserInputModel model)
+        public async Task<bool> RegisterAsync(ApplicationUserInputModel model)
+        {
+            ArgumentNullException.ThrowIfNull(model.Email);
+            ArgumentNullException.ThrowIfNull(model.Password);
+
+            var user = new ApplicationUser
+            {
+                Email = model.Email,
+
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Gender = model.Gender,
+                DateOfBirth = model.DateOfBirth,
+                DateOfRegistration = DateTime.UtcNow
+            };
+
+            var result = await userManager.CreateAsync(user);
+            return result.Succeeded ? true : throw new Exception("Unable to create user; Errors: " + result.Errors);
+        }
+
+        public async Task<bool> ForgotPasswordAsync(ApplicationUserInputModel model)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> LoginAsync(ApplicationUserInputModel model)
+        public async Task<bool> ResetPasswordAsync(ApplicationUserInputModel model)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RefreshTokenAsync(ApplicationUserInputModel model)
+        public async Task<bool> ChangePasswordAsync(ApplicationUserInputModel model)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RegisterAsync(ApplicationUserInputModel model)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> ResetPasswordAsync(ApplicationUserInputModel model)
+        public async Task<bool> RefreshTokenAsync(ApplicationUserInputModel model)
         {
             throw new NotImplementedException();
         }
